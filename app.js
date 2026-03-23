071b8c28c9173cb5f20bb4fbb2fff3f0c7705043
guloghlanqabil-web <guloghlanqabil@gmail.com>
2026-03-23 13:46:46 +0000

    asmet1

--- app.js
+++ app.js
@@ -1,4 +1,37 @@
 "use strict";
 $(document).ready(function () {
 
+    $('.btn').click(function (e) {
+        e.preventDefault();
+
+        let a = $.trim($('input').val());
+        $('input').val('');
+        $('.miniboxes').html('');
+        
+
+        if (!isNaN(a)) {
+
+            for (let i = 1; i <= a; i++) {
+                $('.miniboxes').append(`
+                    <div class="minibox d-flex justify-content-center align-items-center mt-3">
+                        <h2>${a}</h2>
+                    </div>
+                `);
+                
+            }
+
+        } else {
+
+            $('.miniboxes').append(`
+                <div class="minibox d-flex justify-content-center align-items-center mt-3">
+                    <h2>${a}</h2>
+                </div>
+            `);
+            
+        }
+
+
+    });
+
+
 });
