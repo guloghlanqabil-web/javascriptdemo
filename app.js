9d36fcaea18d08c34d99e67f546ce353f778014c
guloghlanqabil-web <guloghlanqabil@gmail.com>
2026-04-03 19:16:39 +0000

    asmet1

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
             let a = b => {
                 for (let i = 1; i <= b; i++) { $('.minibox').append(`<h2>${b}</h2>`); }
             }
-            let v = f => Number(f) ? a(Number(f)) : $('.minibox').append(`<h2>${f}</h2>`);
+            let v = f => Number(f) ? a((f) : $('.minibox').append(`<h2>${f}</h2>`);
             v($('input').val());
         }
         $('input').val('');
